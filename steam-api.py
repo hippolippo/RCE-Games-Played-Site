78220e3de594a382f3e14f56eef55b8c517045f2
Hippolippo <441727wentgo>
2022-02-01 21:06:23 -0500

    can get the image for games and list of games

---
+++ steam-api.py
@@ -0,0 +1,38 @@
+import requests
+
+API_KEY = ""
+USER_ID = "76561198333972976"
+
+class Game:
+
+    def __init__(self, gamedata):
+        self.appid = gamedata['appid']  if 'appid' in gamedata else None
+        self.name = gamedata['name']  if 'name' in gamedata else None
+        self.playtime_forever = gamedata['playtime_forever'] if 'playtime_forever' in gamedata else None
+        self.img_icon_url = gamedata['img_icon_url'] if 'img_icon_url' in gamedata else None
+        self.img_logo_url = gamedata['img_logo_url'] if 'img_logo_url' in gamedata else None
+        self.has_community_visible_stats = gamedata['has_community_visible_stats'] if 'has_community_visible_stats' in gamedata else None
+        self.playtime_windows_forever = gamedata['playtime_windows_forever'] if 'playtime_windows_forever' in gamedata else None
+        self.playtime_mac_forever = gamedata['playtime_mac_forever'] if 'playtime_mac_forever' in gamedata else None
+        self.playtime_linux_forever = gamedata['playtime_linux_forever'] if 'playtime_linux_forever' in gamedata else None
+
+def json_request(path, flags=[]):
+    request = requests.get("http://api.steampowered.com" + path+"?key=" + API_KEY + "&steamid=" + USER_ID + "&".join([""] + flags) + "&format=json")
+    if request.status_code == 200:
+        return request.json()
+
+def json_request_general(path, flags=[]):
+    request = requests.get("http://api.steampowered.com" + path+"?key=" + API_KEY  + "&".join([""] + flags) + "&format=json")
+    if request.status_code == 200:
+        return request.json()
+
+def get_logo_url(game: Game):
+    if(game.appid is None or game.img_icon_url is None):
+        return None
+    return "http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{hash}.jpg".format(appid=game.appid, hash=game.img_logo_url)
+
+
+data = json_request("/IPlayerService/GetOwnedGames/v0001/", ["include_appinfo=true"])
+game_data = data['response']['games']
+games = [Game(info) for info in game_data]
+print(get_logo_url(games[3]), games[3].playtime_forever)
