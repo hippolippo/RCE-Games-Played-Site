8a0b8125bba3ad08cf5afe8eab2002083fb73c19
Hippolippo <441727wentgo>
2022-02-02 19:17:18 -0500

    random

--- youtube_api.py
+++ youtube_api.py
@@ -19,20 +19,44 @@ API_KEY = ""
 CHANNEL_ID = "UCeP4Yv3s4RvS0-6d9OInRMw"
 
 class YoutubeGame(generic.Game):
-    pass
+
+    videos = []
+
+    def add_video(self, video):
+        self.videos.append(video)
+
+    def list_videos(self):
+        return self.videos
+
 class YoutubeGameList(generic.GameList):
-    pass
+
+    def append(self, game):
+        self.game_list.append(game)
+
 
 class YoutubeVideo:
     
     def __init__(self, json):
+        print("-")
         self.id = json["id"]["videoId"]
         self.gameName = self.get_game_name()
+        self.game = initialize_game(self.gameName, self)
         self.title = json["snippet"]["title"]
+        print("|")
 
     def get_game_name(self):
         request = requests.get(f"https://www.youtube.com/watch?v={self.id}")
-        return re.findall("""(?<=,"title":\{"simpleText":").*?(?="},"subtitle")""", request.content.decode("utf-8"))[0].split("\"},\"callToAction")[0]
+        return re.findall("""(?<=,"title":\{"simpleText":").*?(?="},"subtitle")""", request.content.decode("utf-8"))[0].split("\"}")[0]
+
+YoutubeGames = YoutubeGameList([]);
+
+def initialize_game(gameName, video):
+    global YoutubeGames
+    if True not in [gameName == game.get_game_name() for game in YoutubeGames.game_list]:
+        YoutubeGames.append(YoutubeGame(gameName))
+    game = YoutubeGames.game_list[[gameName == game.get_game_name() for game in YoutubeGames.game_list].index(True)]
+    game.add_video(video)
+    return game
 
 # Make a request to a specific api path including the user id
 # DO NOT PUT USER INPUT INTO THE PARAMETERS OF THIS METHOD
@@ -64,9 +88,12 @@ def get_all_videos():
     videos = get_all_videos()
     print(YoutubeVideo(videos[1]).gameName)
     json.dump(videos, file)"""
-
-def get_game_name(id):
-    request = requests.get(f"https://www.youtube.com/watch?v={id}")
-    return re.findall("""(?<=,"title":\{"simpleText":").*?(?="},"subtitle")""", request.content.decode("utf-8"))[0].split("\"},\"callToAction")[0]
-
-print(get_game_name("Ry9SXNCkwuk"))
+"""
+vids = get_all_videos()
+with open("stuff", "w") as file:
+    json.dump(vids, file)
+"""
+with open("stuff", "r") as file:
+    vids = json.load(file)
+videos = [YoutubeVideo(vid) for vid in vids]
+print([item.get_game_name() for item in YoutubeGames.game_list])
