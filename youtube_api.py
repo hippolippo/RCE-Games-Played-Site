c5f80ecd261433e5bd57b282b2b788982dd8aeb4
Hippolippo <441727wentgo>
2022-02-05 19:21:03 -0500

    added api syncer (untested)

--- youtube_api.py
+++ youtube_api.py
@@ -6,6 +6,7 @@ All Code is written by Hippolippo and is licensed through the Creative Commons L
 # A module to serve the purpose of gathering data about the videos made by a channel
 
 # Project Modules
+from numpy import load
 import generic
 
 # Other Modules
@@ -14,7 +15,7 @@ import json
 import re
 
 # Youtube API Key. Do Not Share
-API_KEY = ""
+API_KEY = "AIzaSyBN1Wn7EikreWMnNgynpSizYVbY4wJjlLE"
 # The id of the channel that this will be applied to (RCE)
 CHANNEL_ID = "UCeP4Yv3s4RvS0-6d9OInRMw"
 
@@ -32,17 +33,19 @@ class YoutubeGameList(generic.GameList):
 
     def append(self, game):
         self.game_list.append(game)
+        
 
 
 class YoutubeVideo:
     
     def __init__(self, json):
-        print("-")
         self.id = json["id"]["videoId"]
         self.gameName = self.get_game_name()
         self.game = initialize_game(self.gameName, self)
         self.title = json["snippet"]["title"]
-        print("|")
+        self.thumbnail = json["snippet"]["thumbnails"]["high"]
+        self.date = json["snippet"]["publishTime"]
+        
 
     def get_game_name(self):
         overrides = json.load(open("video_game_override.json", "r"))["videos"]
@@ -52,8 +55,10 @@ class YoutubeVideo:
         if self.id in cache:
             game_name = cache[self.id]
         else:
+            print(f"Downloading game name for video with id {self.id}")
             request = requests.get(f"https://www.youtube.com/watch?v={self.id}")
             game_name = re.findall("""(?<=,"title":\{"simpleText":").*?(?="},"subtitle")""", request.content.decode("utf-8"))[0].split("\"}")[0]
+            print(f"Found: {game_name}")
             cache[self.id] = game_name
             with open("video_game_cache.json", "w") as file:
                 json.dump(cache, file)
@@ -62,6 +67,7 @@ class YoutubeVideo:
             return overrides[game_name]
         else:
             return game_name
+    
 
 YoutubeGames = YoutubeGameList([]);
 
@@ -76,9 +82,9 @@ def initialize_game(gameName, video):
 # Make a request to a specific api path including the user id
 # DO NOT PUT USER INPUT INTO THE PARAMETERS OF THIS METHOD
 def json_request(path, flags=[]):
+    print(f"making request to {path} with flags {flags}")
     request = requests.get("https://www.googleapis.com/youtube/v3" + path + "?key=" + API_KEY + "&channelId=" + CHANNEL_ID + "&".join([""] + flags))
     if request.status_code == 200:
-        print("suc")
         return request.json()
     print(request.json())
 
@@ -97,7 +103,34 @@ def get_all_videos():
     while "nextPageToken" in result:
         result = json_request("/search", ["order=date","part=snippet","type=video","max_results=50", f"pageToken={result['nextPageToken']}"])
         items += result["items"]
-    return items
+    return sorted(items,key= lambda x: x["snippet"]["publishTime"])
+
+def update_videos(current_list):
+    result = json_request("/search", ["order=date","part=snippet","type=video","max_results=10"])
+    amount_added = 0
+    for i in result["items"]:
+        print(i['id']['videoId']+":", i['snippet']['title'])
+        if i['id']['videoId'] not in [item['id']['videoId'] for item in current_list]:
+            amount_added += 1
+            current_list.append(i)
+            print(i)
+    while amount_added >= 10 and "nextPageToken" in result:
+        result = json_request("/search", ["order=date","part=snippet","type=video","max_results=10", f"pageToken={result['nextPageToken']}"])
+        amount_added = 0
+        for i in result["items"]:
+            if i['id']['videoId'] not in [item['id']['videoId'] for item in current_list]:
+                amount_added += 1
+                current_list.append(i)
+                print(i)
+    return current_list
+
+def load_videos_from_cache():
+    with open("video_data_cache.json", "r") as file:
+        vids = json.load(file)
+    vids = sorted(vids, key= lambda x: x["snippet"]["publishTime"])
+    return vids
+    
+    
         
 """with open("stuff", "w") as file:
     videos = get_all_videos()
@@ -108,7 +141,12 @@ vids = get_all_videos()
 with open("stuff", "w") as file:
     json.dump(vids, file)
 """
-with open("stuff", "r") as file:
-    vids = json.load(file)
+vids = load_videos_from_cache()
+should_update = False
+if should_update:
+    update_videos(vids)
+    with open("video_data_cache.json", "w") as file:
+        json.dump(vids, file)
+
 videos = [YoutubeVideo(vid) for vid in vids]
-print([item.get_game_name() for item in YoutubeGames.game_list])
+[print(item.title, end="\n") for item in videos if item.gameName == "Infra"]
